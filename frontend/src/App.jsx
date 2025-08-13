22e838b5edebf751558dba6da0cbbe22de20f697
Prashant Jain <prashantjain0002@gmail.com>
2025-08-13 11:16:38 +0530

    🛠️ Initialize frontend project, install required packages, and configure Tailwind CSS

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1 className='text-red-500 underline font-semibold'>Vite + React</h1>
+      
+    </>
+  )
+}
+
+export default App
